f383a03630592c2ac4135f28b4b81b06d7382d2b
Wadzon <wadzon@local>
2026-06-06 17:18:05 +0500

    Track outbound clicks in Yandex Metrika

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ document.querySelectorAll('[data-pending="true"]').forEach((link) => {
     event.preventDefault();
   });
 });
+
+document.querySelectorAll("[data-metrika-goal]").forEach((link) => {
+  link.addEventListener("click", () => {
+    if (typeof window.ym !== "function") {
+      return;
+    }
+
+    link.dataset.metrikaGoal
+      .split(" ")
+      .filter(Boolean)
+      .forEach((goal) => {
+        window.ym(109692355, "reachGoal", goal);
+      });
+  });
+});
